3a63c2837a4d6495aa38aaf92095a155acd06744
Alexis <alexander17179@gmail.com>
2026-08-01 17:46:21 -0600

    Fix:Auth

--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class AuthService {
-  private apiUrl = 'https://frontend-pr4v1ybqp-aelx.vercel.app//api/auth';
+  private apiUrl = 'https://frontend-pr4v1ybqp-aelx.vercel.app/api/auth';
 
   constructor(private http: HttpClient) {}
 
