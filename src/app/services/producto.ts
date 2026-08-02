0419a2f502c92f0f6a4fb11b17fea446fabbfcb3
Alexis <alexander17179@gmail.com>
2026-08-01 18:56:42 -0600

    Fix

--- src/app/services/producto.ts
+++ src/app/services/producto.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import {CommonModule} from '@angular/common';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
