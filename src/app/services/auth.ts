8ea734c0a06d0bdce6b7e99b59ef32e608fe3307
Alexis <alexander17179@gmail.com>
2026-08-01 19:06:34 -0600

    Fix

--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  private apiUrl = `${environment.apiUrl}/api/auth`;
+  private apiUrl = 'backend-app-production-8657.up.railway.app/api/auth';
 
   constructor(private http: HttpClient) {}
 
